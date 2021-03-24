d92b1cc5d8579d665f4d980471f0d78df03151e9
blurryroots <blurryroots@posteo.de>
2021-03-24 17:27:13 +0100

    added remap, fbx and fbx-list commands as prep for verwandlung integration, unified frame reading

--- src/llv/cli.py
+++ src/llv/cli.py
@@ -12,35 +12,82 @@ import time
 import argparse
 import json
 import base64
-from .gesicht import FaceFrame, remap
-from .buchse import Buchse
 import struct
 import os
 import gzip
+import csv
+import math
+from .gesicht import FaceFrame, remap
+from .buchse import Buchse
+from .__init__ import version as get_version
+
+
+def tween(a, b, frames):
+    if 2 > frames:
+        return [b]
+    diff = b - a
+    step = diff / (frames - 1)
+    return [a + i * step for i in range(0, frames)]
+
 
 def clamp(value, min_value, max_value):
    return max(min(value, max_value), min_value)
 
 
-def read_frames(filepath, loop = False):
+def is_binary_file(file_name):
+    """
+    Tries to open file in text mode and read first 64 bytes. If it fails,
+    we can be fairly certain, this is due to the file being binary encoded.
+    Thanks Sehrii https://stackoverflow.com/a/51495076/949561 for the help.
+    """
+    try:
+        with open(file_name, 'tr') as check_file:
+            check_file.read(32)
+            return False
+    except:
+        return True
+
+
+def _read_frames_json(filepath):
+    with open(filepath, 'r', encoding='utf-8', newline='\r\n') as f:
+        recording_json = json.load(f)
+
+        frame_count = recording_json['count']
+        frame_index = 0
+
+        for frame_json in recording_json['frames']:
+            yield frame_json, frame_index, frame_count, frame_json['version']
+            frame_index += 1
+
+
+def _read_frames_binary(filepath):
+    with gzip.open(filepath, 'rb') as file:
+        version, = struct.unpack('>B', file.read(1))
+        if version != FaceFrame.VERSION:
+            raise Exception(f'Incompatible frame versions! Recording is at {version}, llv at {FaceFrame.VERSION}.')
+        frame_count, = struct.unpack('>L', file.read(4))
+
+        for frame_index in range(0, frame_count):
+            raw_frame_size = file.read(4)
+            frame_size, = struct.unpack('>L', raw_frame_size)
+            frame_data = file.read(frame_size)
+
+            yield frame_data, frame_index, frame_count, version
+
+
+def read_frames(filepath, is_binary = True, loop = False):
     file_size = os.path.getsize(filepath)
+
+    if is_binary:
+        frame_generator = _read_frames_binary(filepath)
+    else:
+        frame_generator = _read_frames_json(filepath)
 
     keep_reading = True
     while keep_reading:
-        with gzip.open(filepath, 'rb') as file:
-            file.seek(0, 0)
-
-            version, = struct.unpack('>B', file.read(1))
-            if version != FaceFrame.VERSION:
-                raise Exception(f'Incompatible frame versions! Recording is at {version}, llv at {FaceFrame.VERSION}.')
-            frame_count, = struct.unpack('>L', file.read(4))
-
-            for frame_index in range(0, frame_count):
-                raw_frame_size = file.read(4)
-                frame_size, = struct.unpack('>L', raw_frame_size)
-                frame_data = file.read(frame_size)
-
-                yield frame_data, frame_index, frame_count, version
+        
+        for frame_package in frame_generator:
+            yield frame_package
 
         if not loop:
             keep_reading = False
@@ -56,7 +103,8 @@ def playback(host, port, filepath, fps, loop = True):
 
     frame_index = -1
     frame_count = -1
-    for frame_data, frame_index, frame_count, version in read_frames(filepath, loop):
+    for frame_package in read_frames(filepath, loop):
+        frame_data, frame_index, frame_count, version = frame_package
         if 0 == frame_index:
             print(f'Start sending {frame_count} frames of version {version} @{fps}fps ...')
 
@@ -137,22 +185,24 @@ def unpack(raw_file, output, retain_raw_frame = True, rename = ''):
         file.write(']}')
 
 
-def pack(clear_file, output, rename = ''):
+def pack(clear_filepath, output, rename = ''):
+    print(f'Generating packed recording at {clear_filepath} from clearfile {clear_filepath} ...')
     with gzip.open(output, 'wb') as outfile:
         outfile.write(struct.pack('>B', FaceFrame.VERSION)) # version of the binary protocol
 
-        recording_json = None
-        with open(clear_file, 'r', encoding='utf-8', newline='\r\n') as infile:
-            recording_json = json.load(infile)
+        for frame_json, frame_index, frame_count, version in read_frames(clear_filepath, is_binary = False, loop = False):
+            if 0 == frame_index:
+                # how many frames are in the recording?
+                outfile.write(struct.pack('>L', frame_count))
 
-        outfile.write(struct.pack('>L', recording_json['count'])) # how many frames are in the recording?
-
-        for frame_json in recording_json['frames']:
             if 0 < len(rename):
                 frame_json['subject_name'] = rename
                 frame_json['device_id'] = 'DEADC0DE-1337-1337-1337-CAFEBABE'
+
             frame = FaceFrame.from_json(frame_json)
             outfile.write(frame.encode())
+
+    print(f'Done.')
 
 
 def migrate(legacy_file, output, rename = ''):
@@ -196,6 +246,7 @@ def _write_frames_for_shape(file, shape_name, frames_per_shape, total_number_of_
 
         file.write(frame.encode())
 
+
 def sequence(output, time_per_shape = 1.1, fps = 60, single_shape = '', min_value = -1.0, max_value = 1.0):
     print(f'Requesting debug sequence with {time_per_shape}s per shape @{fps}fps ...')
 
@@ -225,8 +276,143 @@ def sequence(output, time_per_shape = 1.1, fps = 60, single_shape = '', min_valu
     return frames_written
 
 
+def create_modifier(output_path, default_value = 1.0):
+    modifiers = {}
+    for shape_name in FaceFrame.FACE_BLENDSHAPE_NAMES:
+        modifiers[shape_name] = default_value
+
+    with open(output_path, 'w', encoding='utf-8', newline='\r\n') as f:
+        f.write(json.dumps(modifiers))
+
+
+def apply_modifiers(recording_filepath, modifiers_filepath, default_value = 1.0):
+    is_binary_recording = is_binary_file(recording_filepath)
+    for frame_data, frame_index, frame_count, version \
+        in read_frames(recording_filepath, is_binary = is_binary_recording, loop = False):
+        raise Exception('Not implemented yet!')
+
+def create_remap_library(csv_filepath, library_filepath, dialect = 'excel'):
+    mapping = {}
+    modifiers = {}
+    reverse_mapping = {}
+    with open(csv_filepath, 'r') as f:
+        index = -1
+        for row in csv.reader(f, dialect='excel'):
+            index += 1
+            if 0 == index:
+                continue
+            if 'undefined' in row[1].lower():
+                continue
+            if 'undefined' in row[2].lower():
+                continue
+            mapping[row[1]] = row[2]
+            modifiers[row[1]] = float(row[3])
+            reverse_mapping[row[2]] = row[1]
+
+    result = {'mapping': mapping, 'modifiers': modifiers, 'reverse': reverse_mapping}
+
+    with open(library_filepath, 'w', encoding='utf-8', newline='\r\n') as f:
+        f.write(json.dumps(result))
+
+
+def fbx_list(fbx_meta_filepath):
+    with open(fbx_meta_filepath, 'r', encoding='utf-8', newline='\r\n') as f:
+        fbx_metadata = json.load(f)
+        for shape in fbx_metadata['shapes']:
+            print(shape['target'])
+
+
+def fbx_meta(fbx_meta_filepath, library_filepath, output_path):
+    library = None
+    with open(library_filepath, 'r', encoding='utf-8', newline='\r\n') as f:
+        library = json.load(f)
+
+    arr = {}
+    for key in library['reverse'].keys():
+        arr[key.lower()] = key
+
+    shape_values = {}
+    for shape_name in FaceFrame.FACE_BLENDSHAPE_NAMES:
+        shape_values[shape_name] = []
+
+    max_duration = 0
+    max_len_values = 0
+    with open(fbx_meta_filepath, 'r', encoding='utf-8', newline='\r\n') as f:
+        fbx_metadata = json.load(f)
+        for shape in fbx_metadata['shapes']:
+            l = shape['target'].lower()
+            if l in arr:
+                arkit_name = library['reverse'][arr[l]]
+                mesh_name = arr[l]
+                shape_values[arkit_name] = [
+                    library['modifiers'][arkit_name] * value for value in shape['curves'][0]['values']
+                ]
+                max_len_values = max(max_len_values, len(shape_values[arkit_name]))
+                max_duration = max(max_duration, shape['curves'][0]['end_time'] - shape['curves'][0]['start_time'])
+
+    tween_frames = math.floor(60.0 / math.floor(max_len_values / max_duration))
+    if 1 > tween_frames:
+        raise Exception(f'Weirdness! 1 > (60 / ({max_len_values} (max_len_values) / {max_duration} (max_duration)))')
+
+    for name in shape_values:
+        if max_len_values != len(shape_values[name]):
+            shape_values[name] = [0] * max_len_values
+        raw = shape_values[name]
+        raw_length = len(raw)
+        shape_values[name] = []
+        previous_value = -1
+        for i in range(0, raw_length):
+            if 0 == i:
+                previous_value = raw[i]
+                continue
+            shape_values[name] += tween(previous_value, raw[i], tween_frames)
+            previous_value = raw[i]
+
+    options = {'duration': max_duration
+        , 'samples': max_len_values
+        , 'fps': 60
+        , 'tweens': tween_frames}
+
+    print(f'Options: {options}')
+
+    total_number_of_frames = (options['samples']-1) * options['tweens']
+    print(f'Processing {total_number_of_frames} frames ...')
+    with open(output_path, 'w', encoding='utf-8', newline='\r\n') as file:
+        file.write(f'{{"count": {total_number_of_frames}, "frames": [')
+        for frame_index in range(0, total_number_of_frames):
+            frame = FaceFrame.from_default(frame_index)
+            for shape_name in FaceFrame.FACE_BLENDSHAPE_NAMES:
+                shape_value = 0
+                if shape_name in shape_values:
+                    try:
+                        shape_value = shape_values[shape_name][frame_index]
+                    except Exception as e:
+                        print(f'Got {e} when accessing {frame_index} / len {len(shape_values[shape_name])}')
+                        raise e
+                frame.blendshapes[shape_name] = shape_value
+
+            file.write(frame.to_json())
+
+            if frame_index < (total_number_of_frames - 1):
+                file.write(',')
+
+        file.write(']}')
+        
+
+
 def create_arg_parser():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    # Setup global flags for verbosity level and version print.
+    parser.add_argument('-v', '--verbose'
+        , action='store_true'
+        , help='Activate verbose logging.'
+        , default=False)
+    parser.add_argument('-V', '--version'
+        , action='store_true'
+        , help='Show version number.'
+        , default=False)
+
+    # Split object for subparsers.
     subparsers = parser.add_subparsers(dest='command')
 
     # Setup record command and options.
@@ -312,12 +498,37 @@ def create_arg_parser():
         , help='Maximum value for the shape to assume when animating.'
         , default=1.0)
 
+    remap_args = subparsers.add_parser('remap')
+    remap_args.add_argument('csv_filepath', metavar='csv_filepath', type=str
+        , help='Path to csv file with remapping info.')
+    remap_args.add_argument('output_path', metavar='out_path', type=str
+        , help='Path to write mapping file to.')
+    remap_args.add_argument('--dialect', metavar='d', type=str
+        , help='Dialect used in csv file.'
+        , default='excel')
+
+    remap_args = subparsers.add_parser('fbx')
+    remap_args.add_argument('fbx_meta_filepath', metavar='fbx_meta_filepath', type=str
+        , help='Path to fbx blendshape metadata file.')
+    remap_args.add_argument('library_filepath', metavar='library_filepath', type=str
+        , help='Path to blendshape mapping definition file.')
+    remap_args.add_argument('output_path', metavar='out_path', type=str
+        , help='Path to write llv recording file to.')
+
+    remap_args = subparsers.add_parser('fbx-list')
+    remap_args.add_argument('fbx_meta_filepath', metavar='fbx_meta_filepath', type=str
+        , help='Path to fbx blendshape metadata file.')
+
     return parser
 
 
 def main():
     parser = create_arg_parser()
     args = parser.parse_args()
+
+    if args.version:
+        print(get_version())
+        sys.exit(0)
 
     if 'play' == args.command:
         frames_read, frames_total = playback(args.host, args.port, args.recording_path, args.fps)
@@ -334,11 +545,15 @@ def main():
     elif 'sequence' == args.command:
         fps = 60
         sequence(args.output_path, args.time_per_shape, fps, args.single_shape, args.min, args.max)
+    elif 'remap' == args.command:
+        create_remap_library(args.csv_filepath, args.output_path, args.dialect)
+    elif 'fbx' == args.command:
+        fbx_meta(args.fbx_meta_filepath, args.library_filepath, args.output_path)
+    elif 'fbx-list' == args.command:
+        fbx_list(args.fbx_meta_filepath)
     else:
         parser.print_help()
         sys.exit(1)
-
-    print('Shutting down ...')
 
 
 if __name__ == '__main__':
